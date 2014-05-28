1c671134b6f387c9af95a6aa978efe06042f618f
cewing <cris@crisewing.com>
2014-05-27 23:19:29 -0700

    more updates for step 1

--- journal.py
+++ journal.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
+from contextlib import closing
 from flask import Flask
+import os
+import psycopg2
 
 
 DB_SCHEMA = """
@@ -14,6 +17,30 @@ CREATE TABLE entries (
 
 
 app = Flask(__name__)
+
+app.config['DATABASE'] = os.environ.get(
+    'DATABASE_URL', 'dbname=learning_journal user=cewing'
+)
+
+
+def connect_db():
+    """Return a connection to the configured database"""
+    return psycopg2.connect(app.config['DATABASE'])
+
+
+def init_db():
+    """Initialize the database using DB_SCHEMA
+
+    WARNING: executing this function will drop existing tables.
+    """
+    with closing(connect_db()) as db:
+        db.cursor().execute(DB_SCHEMA)
+        db.commit()
+
+
+@app.route('/')
+def hello():
+    return u'Hello world!'
 
 
 if __name__ == '__main__':
