83efc2c2d664870e3bc3031a6c0b692ff733bd4a
cewing <cris@crisewing.com>
2014-05-31 17:20:14 -0700

    implement and test logout

--- journal.py
+++ journal.py
@@ -139,5 +139,11 @@ def login():
     return render_template('login.html', error=error)
 
 
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    return redirect(url_for('show_entries'))
+
+
 if __name__ == '__main__':
     app.run(debug=True)
