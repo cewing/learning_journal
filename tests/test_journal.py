2099c3e9b199c13715c6a9e49e62cf3bc5fff8e9
cewing <cris@crisewing.com>
2015-06-30 21:56:13 -0700

    reorganize tests, add a bdd test

--- tests/test_journal.py
+++ tests/test_journal.py
@@ -17,37 +17,6 @@ os.environ['TESTING'] = "True"
 import journal
 
 
-@pytest.fixture(scope='session')
-def connection(request):
-    engine = create_engine(TEST_DATABASE_URL)
-    journal.Base.metadata.create_all(engine)
-    connection = engine.connect()
-    journal.DBSession.registry.clear()
-    journal.DBSession.configure(bind=connection)
-    journal.Base.metadata.bind = engine
-    request.addfinalizer(journal.Base.metadata.drop_all)
-    return connection
-
-
-@pytest.fixture()
-def db_session(request, connection):
-    from transaction import abort
-    trans = connection.begin()
-    request.addfinalizer(trans.rollback)
-    request.addfinalizer(abort)
-
-    from journal import DBSession
-    return DBSession
-
-
-@pytest.fixture()
-def app(db_session):
-    from journal import main
-    from webtest import TestApp
-    app = main()
-    return TestApp(app)
-
-
 @pytest.fixture()
 def entry(db_session):
     entry = journal.Entry.write(
