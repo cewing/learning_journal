b065072ba68de601db795a24ebad411331cf0ddb
cewing <cris@crisewing.com>
2016-01-26 14:43:15 -0800

    add a minimal setup.py to allow tox to install the code

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+
+setup(
+    name='learning_journal',
+    version='0.1',
+    description='A simple Pyramid learning journal',
+    author='Cris Ewing',
+    author_email='cris@crisewing.com',
+    license='MIT',
+    modules=['journal'],
+    zip_safe=False
+)
